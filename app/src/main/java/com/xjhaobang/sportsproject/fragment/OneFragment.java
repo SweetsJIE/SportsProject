a0c79db7d5ee4a327c14d90e7dec47230126c149
SweestJIE <32554622@qq.com>
2018-05-23 23:16:42 +0800

    创青春自行车app，完成部分功能，蓝牙调试

--- app/src/main/java/com/xjhaobang/sportsproject/fragment/OneFragment.java
+++ app/src/main/java/com/xjhaobang/sportsproject/fragment/OneFragment.java
@@ -47,6 +47,9 @@ public class OneFragment extends BaseFragment {
 
     }
 
+
+
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
